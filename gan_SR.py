c0952d0f0c84e9a93d47bf5eed61f9e442167975
Yuguang Tong <ygtong@berkeley.edu>
2017-04-29 23:54:39 +0000

    add L1 difference to loss function

--- gan_SR.py
+++ gan_SR.py
@@ -36,6 +36,7 @@ ITERS = 5000 # How many iterations to train for
 LAMBDA = 10 # Gradient penalty lambda hyperparameter
 INPUT_DIM = 16*16*3 # Number of pixels in each input
 OUTPUT_DIM = 64*64*3 # Number of pixels in each iamge
+GEN_L1_WEIGHT = 0.9 # Weighting factor for L1 difference in generator loss
 
 lib.print_model_settings(locals().copy())
 def GeneratorAndDiscriminator():
@@ -158,19 +159,36 @@ def FCGenerator(n_samples, noise=None, FC_DIM=512, input_dim=INPUT_DIM):
 
 def DCGANGenerator(
         n_samples, noise=None, dim=DIM, input_dim=INPUT_DIM,
-        bn=True, nonlinearity=tf.nn.relu):
+        k=K, bn=True, nonlinearity=tf.nn.relu):
+
     lib.ops.conv2d.set_weights_stdev(0.02)
     lib.ops.deconv2d.set_weights_stdev(0.02)
     lib.ops.linear.set_weights_stdev(0.02)
-
+    
     if noise is None:
-        noise = tf.random_normal([n_samples, 256])
-
-    output = lib.ops.linear.Linear('Generator.Input', 256, 4*4*8*dim, noise)
-    output = tf.reshape(output, [-1, 8*dim, 4, 4])
-    if bn:
-        output = Batchnorm('Generator.BN1', [0,2,3], output)
-    output = nonlinearity(output)
+        noise = tf.random_normal([n_samples, input_dim])
+        output = lib.ops.linear.Linear(
+            'Generator.Input', 256, (dim//k)*(dim//k)*8*dim, noise)
+        output = tf.reshape(output, [-1, 8*dim, dim//k, dim//k])
+        if bn:
+            output = Batchnorm('Generator.BN1', [0,2,3], output)
+            output = nonlinearity(output)
+    else:
+        # downsampled data as input (noise)
+        # input (noise) dimension [batchsize, 3*(dim/K)*(dim/K)]
+        # decode twice to tensor of [batchsize, 8*dim, 4, 4]
+        output = tf.reshape(noise, [-1, 3, dim//k, dim//k])
+        output = tflib.ops.conv2d.Conv2D(
+            'Generator.Encoder1.1', 3, 4*dim, 5, output, stride=2)
+        if bn:
+            output = Batchnorm('Generator.BN1.1', [0,2,3], output)
+            output = nonlinearity(output)
+            
+        output = tflib.ops.conv2d.Conv2D(
+            'Generator.Encode1.2', 4*dim, 8*dim, 5, output, stride=2)
+        if bn:
+            output = Batchnorm('Generator.BN1.2', [0, 2, 3], output)
+            output = nonlinearity(output)
 
     output = lib.ops.deconv2d.Deconv2D('Generator.2', 8*dim, 4*dim, 5, output)
     if bn:
@@ -189,7 +207,7 @@ def DCGANGenerator(
 
     output = lib.ops.deconv2d.Deconv2D('Generator.5', dim, 3, 5, output)
     output = tf.tanh(output)
-
+    
     lib.ops.conv2d.unset_weights_stdev()
     lib.ops.deconv2d.unset_weights_stdev()
     lib.ops.linear.unset_weights_stdev()
@@ -373,6 +391,16 @@ def DCGANDiscriminator(inputs, dim=DIM, bn=True, nonlinearity=LeakyReLU):
 
     return tf.reshape(output, [-1])
 
+def downsample(data, dim=DIM, k=K):
+    data = tf.reshape(data, [-1, 3, dim, dim])
+    # BCHW -> BHWC
+    data = tf.transpose(data, [0, 2, 3, 1])
+    data = tf.image.resize_images(data, [dim//k, dim//k])
+    # BHWC -> BCHW
+    data = tf.transpose(data, [0, 3, 1, 2])
+    data = tf.reshape(data, [-1, 3 * dim//k * dim//k])
+    return data
+
 Generator, Discriminator = GeneratorAndDiscriminator()
 
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
@@ -386,14 +414,11 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
 
     for device_index, (device, real_data_conv) in enumerate(zip(DEVICES, split_real_data_conv)):
         with tf.device(device):
-
-            real_data = tf.reshape(2*((tf.cast(real_data_conv, tf.float32)/255.)-.5), [BATCH_SIZE//len(DEVICES), OUTPUT_DIM])
-            # 
-            real_data_reshape = tf.image.transpose(real_data, [0, 2, 3, 1])
-            
-            real_data_downsampled = tf.image.resize_area(
-                real_data_reshape, [DIM//K, DIM//K])
-            fake_data = Generator(BATCH_SIZE//len(DEVICES))
+            real_data = 2*((tf.cast(real_data_conv, tf.float32)/255.)-.5)
+            real_data = tf.reshape(real_data, [BATCH_SIZE//len(DEVICES), OUTPUT_DIM])
+            # downsampled (by K) as generator input
+            real_data_downsampled = downsample(real_data)
+            fake_data = Generator(BATCH_SIZE//len(DEVICES), noise=real_data_downsampled)
             
             disc_real = Discriminator(real_data)
             disc_fake = Discriminator(fake_data)
@@ -439,6 +464,12 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
             else:
                 raise Exception()
 
+            # add L1 difference to penalty
+            fake_data_downsampled = downsample(fake_data)
+            gen_l1_cost = tf.reduce_mean(
+                tf.abs(fake_data_downsampled - real_data_downsampled))
+            gen_cost = GEN_L1_WEIGHT * gen_l1_cost + (1 - GEN_L1_WEIGHT) * gen_cost
+            
             gen_costs.append(gen_cost)
             disc_costs.append(disc_cost)
 
@@ -448,8 +479,8 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
     tf.summary.scalar('disc loss', disc_cost, collections=['scalars'])
 
     if MODE == 'wgan':
-        gen_train_op = tf.train.RMSPropOptimizer(learning_rate=5e-5).minimize(gen_cost,
-                                             var_list=lib.params_with_name('Generator'), colocate_gradients_with_ops=True)
+        gen_train_op = tf.train.RMSPropOptimizer(learning_rate=5e-5).minimize(
+            gen_cost, var_list=lib.params_with_name('Generator'), colocate_gradients_with_ops=True)
         disc_train_op = tf.train.RMSPropOptimizer(learning_rate=5e-5).minimize(disc_cost,
                                              var_list=lib.params_with_name('Discriminator.'), colocate_gradients_with_ops=True)
 
@@ -460,8 +491,9 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
         clip_disc_weights = tf.group(*clip_ops)
 
     elif MODE == 'wgan-gp':
-        gen_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(gen_cost,
-                                          var_list=lib.params_with_name('Generator'), colocate_gradients_with_ops=True)
+        gen_train_op = tf.train.AdamOptimizer(
+            learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(
+                gen_cost,var_list=lib.params_with_name('Generator'), colocate_gradients_with_ops=True)
         disc_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(disc_cost,
                                            var_list=lib.params_with_name('Discriminator.'), colocate_gradients_with_ops=True)
 
@@ -481,7 +513,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
         raise Exception()
 
     # For generating samples
-    fixed_noise = tf.constant(np.random.normal(size=(BATCH_SIZE, 128)).astype('float32'))
+    fixed_noise = tf.constant(np.random.normal(size=(BATCH_SIZE, INPUT_DIM)).astype('float32'))
     all_fixed_noise_samples = []
     for device_index, device in enumerate(DEVICES):
         n_samples = BATCH_SIZE // len(DEVICES)
@@ -533,7 +565,7 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
 
         # Train generator
         if iteration > 0:
-            _ = session.run(gen_train_op)
+            _ = session.run(gen_train_op, feed_dict={all_real_data_conv: _data})
 
         # Train critic
         if (MODE == 'dcgan') or (MODE == 'lsgan'):
